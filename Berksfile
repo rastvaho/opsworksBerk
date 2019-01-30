52dec316525109916d5566f8926c24dc9e8e8845
Rasti <rastvaho@amazon.com>
2019-01-31 10:30:55 +1100

    initial commit

---
+++ Berksfile
@@ -0,0 +1,4 @@
+source "https://supermarket.chef.io"
+
+cookbook 'yum-docker', '~> 0.3.0'
+cookbook 'docker-nginx', '~> 0.3.0'
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+file '/etc/aws/opsworks/instance-agent-test.yml' do
+  content 'This is a test file'
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
